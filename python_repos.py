c87d12c507617cbae14b66573d4dfa7a7cc1b705
xgy221 <17302558812@163.com>
2018-04-12 12:26:23 +0800

    pygal

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # 执行API调用并存储响应
 url = 'https://api.github.com/search/repositories?q=language:python&sort=starts'
@@ -7,20 +9,30 @@ print("Status code:", r.status_code)
 
 # 将API响应存储在一个变量中
 response_dict = r.json()
-print("Total repositories:",response_dict['total_count'])
+print("Total repositories:", response_dict['total_count'])
 
-#探索有关仓库的信息
+# 探索有关仓库的信息
 repo_dicts = response_dict['items']
-print("Repositories returned:",len(repo_dicts))
 
-#研究第一个仓库
-repo_dict = repo_dicts[0]
-print("\nSelected information about repository:")
-print('Name:',repo_dict['name'])
-print('Owner:',repo_dict['owner']['login'])
-print('Stars:',repo_dict['stargazers_count'])
-print('Repository:',repo_dict['html_url'])
-print('Created:',repo_dict['created_at'])
-print('Updated:',repo_dict['updated_at'])
-print('Description:',repo_dict['description'])
+names, stars = [], []
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
 
+# 可视化
+my_style = LS('#333366', base_style=LCS)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend=False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config,style=my_style)
+chart.title = 'Most-Starred Python Projects on Github'
+chart.x_labels = names
+chart.add('', stars)
+chart.render_to_file('Python_repos.svg')
