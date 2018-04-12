2499cc1a23eefec664fcf0b0e831c19e0623d5c6
xgy221 <17302558812@163.com>
2018-04-12 14:37:55 +0800

    add

--- python_repos.py
+++ python_repos.py
@@ -14,16 +14,24 @@ print("Total repositories:", response_dict['total_count'])
 # 探索有关仓库的信息
 repo_dicts = response_dict['items']
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+        'xlink':repo_dict['html_url']
+    }
+    if plot_dict['label'] is None:
+        plot_dict['label'] = ''
+    plot_dicts.append(plot_dict)
 
 # 可视化
 my_style = LS('#333366', base_style=LCS)
 my_config = pygal.Config()
 my_config.x_label_rotation = 45
-my_config.show_legend=False
+my_config.show_legend = False
 my_config.title_font_size = 24
 my_config.label_font_size = 14
 my_config.major_label_font_size = 18
@@ -31,8 +39,8 @@ my_config.truncate_label = 15
 my_config.show_y_guides = False
 my_config.width = 1000
 
-chart = pygal.Bar(my_config,style=my_style)
+chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on Github'
 chart.x_labels = names
-chart.add('', stars)
-chart.render_to_file('Python_repos.svg')
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos.svg')
